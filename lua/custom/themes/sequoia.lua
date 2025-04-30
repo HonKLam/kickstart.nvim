a799fca74a72960942be2466fc7075e6b8d161e9
Lamo <hl_nguyen@outlook.de>
2025-04-30 20:20:55 +0200

    edit sequioa theme

--- lua/custom/themes/sequoia.lua
+++ lua/custom/themes/sequoia.lua
@@ -3,6 +3,6 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    vim.cmd.colorscheme 'sequoia' -- or 'sequoia-night' / 'sequoia-rise'
+    vim.cmd.colorscheme 'sequoia-insomnia' -- or 'sequoia-night' / 'sequoia-rise'
   end,
 }
